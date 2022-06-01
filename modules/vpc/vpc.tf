839398c219b96698272da8c5d57db12ec4fab235
Joel Carpenter <aldosac@hotmail.com>
2022-06-01 15:42:42 -0700

    Adds version constraint to vpc module

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
+  version                = "~> 3.0"
   name                   = "terraform-vpc"
   cidr                   = var.cidr
   azs                    = var.azs
