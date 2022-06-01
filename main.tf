3a5ba48479ab4445f49172ac85822c0f381c341c
Joel Carpenter <aldosac@hotmail.com>
2022-06-01 15:15:07 -0700

    Adds main.tf and variables.tf in root

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source          = "./vpc"
+  cidr            = var.cidr
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+}
